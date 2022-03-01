46d8114545e7e5a46336d998ce9f1c0cfc157bdf
NguyenTam2206 <55978864+NguyenTam2206@users.noreply.github.com>
2022-03-01 13:26:37 +0700

    1,2,3
    add test file
    
    test squash 2 commits
    
    test commit git desktop

--- components/Report/index.vue
+++ components/Report/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>This is report page</div>
+  <div>
+    <div>one</div>
+    <div>two</div>
+    <div>three</div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +11,8 @@ export default {
   created() {
     this.$axios({
       url: `${process.env.baseAPI}/result`,
-      method: "GET"
-    }).then(res => console.log("res", res.data));
-  }
+      method: "GET",
+    }).then((res) => console.log("res", res.data));
+  },
 };
 </script>
